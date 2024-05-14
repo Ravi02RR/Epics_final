5b8afe9809fbb638845a635b660a13160ab31295
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-05-14 12:58:42 +0530

    fix: :bug: exposed key

--- Client/src/Component/Chatbot/ChatBot.jsx
+++ Client/src/Component/Chatbot/ChatBot.jsx
@@ -14,7 +14,8 @@ const ChatBot = () => {
     const LOGO = "https://w7.pngwing.com/pngs/296/534/png-transparent-robot-cute-robot-blue-electronics-humanoid-robot-thumbnail.png";
     const USERICON = "https://cdn-icons-png.flaticon.com/512/149/149071.png";
 
-    const apiKey ="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";
+    const apiKey =import.meta.env.VITE_SOME_KEY;
+    console.log(apiKey)
     console.log("api key",apiKey)
 
     const chatContainerRef = useRef(null);
